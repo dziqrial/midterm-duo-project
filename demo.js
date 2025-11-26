4a8d13be692320737a6bd202547d603037b9b5f2
Durrahman99 <mamatsuhardi99@gmail.com>
2025-11-26 18:26:35 +0700

    menegdit halaman program

---
+++ demo.js
@@ -0,0 +1,18 @@
+// const images = document.querySelectorAll(".fotoprogram img");
+
+// function revealOnScroll() {
+//     const trigger = window.innerHeight * 0.85;
+
+//     images.forEach(img => {
+//         const pos = img.getBoundingClientRect().top;
+
+//         if (pos < trigger) {
+//             img.classList.add("show");
+//         } else {
+//             img.classList.remove("show");
+//         }
+//     });
+// }
+
+// window.addEventListener("scroll", revealOnScroll);
+// revealOnScroll();
